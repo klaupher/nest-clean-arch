4e92c81668c23ff01f8feaec1fb4365bff3518f7
Fernanda Bernardes <fernanda.bernardes@gsoftware.com.br>
2023-12-11 15:50:48 -0300

    Update methods update tests

--- src/users/domain/entities/__tests__/unit/unit.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/unit.entity.spec.ts
@@ -17,6 +17,18 @@ describe(' UserEntity unit test', () => {
     expect(sut.props.createdAt).toBeInstanceOf(Date);
   });
 
+  it('Getter of name field', () => {
+    expect(sut.props.name).toBeDefined();
+    expect(sut.props.name).toEqual(props.name);
+    expect(typeof sut.props.name).toBe('string');
+  });
+
+  it('Setter of name field', () => {
+    sut['name'] = 'other name';
+    expect(sut.props.name).toEqual('other name');
+    expect(typeof sut.props.name).toBe('string');
+  });
+
   it('Getter of email field', () => {
     expect(sut.props.email).toBeDefined();
     expect(sut.props.email).toEqual(props.email);
@@ -29,8 +41,24 @@ describe(' UserEntity unit test', () => {
     expect(typeof sut.props.password).toBe('string');
   });
 
+  it('Setter of password field', () => {
+    sut['password'] = 'other password';
+    expect(sut.props.password).toEqual('other password');
+    expect(typeof sut.props.password).toBe('string');
+  });
+
   it('Getter of createdAt field', () => {
     expect(sut.props.createdAt).toBeDefined();
     expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
+
+  it('Should update a user name', () => {
+    sut.update('other name');
+    expect(sut.props.name).toEqual('other name');
+  });
+
+  it('Should update a user password', () => {
+    sut.updatePass('other password');
+    expect(sut.props.password).toEqual('other password');
   });
 });
--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -16,8 +16,19 @@ export class UserEntity extends BaseEntity<UserProps> {
     this.props.createdAt = this.props.createdAt ?? new Date();
   }
 
+  update(value: string): void {
+    this.name = value;
+  }
+
+  updatePass(value: string): void {
+    this.password = value;
+  }
+
   get name() {
     return this.props.name;
+  }
+  private set name(value: string) {
+    this.props.name = value;
   }
 
   get email() {
@@ -26,6 +37,10 @@ export class UserEntity extends BaseEntity<UserProps> {
 
   get password() {
     return this.props.password;
+  }
+
+  private set password(value: string) {
+    this.props.password = value;
   }
 
   get createdAt() {
